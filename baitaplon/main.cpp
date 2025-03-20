215485711f2f916fc1d80bcf3835e78940a81207
PhamGiaHien <phamgiahien1205@gmail.com>
2025-03-20 22:05:39 +0700

    test file

--- baitaplon/main.cpp
+++ baitaplon/main.cpp
@@ -1,78 +1,24 @@
 #include <iostream>
 #include <SDL.h>
-#include <SDL_image.h>
-#include "init_screen.h"
-#include "defs.h"
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 using namespace std;
 
-void waitUntilKeyPressed()
-{
+void wait_until_exit(){
     SDL_Event e;
-    while (true) {
-        if ( SDL_PollEvent(&e) != 0 &&
-             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
-            return;
-        SDL_Delay(100);
-    }
-}
+    bool running = true;
 
-int main(int argc, char *argv[])
-{
-    Graphics graphics;
-    graphics.init();
-
-    bool quit = false;
-    SDL_Event event;
-    while (!quit) {
-        //Handle events on queue
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) quit = true;
-        }
-
-        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-        if (currentKeyStates[SDL_SCANCODE_UP]) cerr << " Up";
-        if (currentKeyStates[SDL_SCANCODE_DOWN]) cerr << " Down";
-        if (currentKeyStates[SDL_SCANCODE_LEFT]) cerr << " Left";
-        if (currentKeyStates[SDL_SCANCODE_RIGHT]) cerr << " Right";
-
-        cerr << ".\n";
-
-        SDL_Delay(100);
-    }
-
-    graphics.quit();
-    return 0;
-}
-
-/*
-//thử nghiệm scancode
-int main(int argc, char *argv[])
-{
-    Graphics graphics;
-    graphics.init();
-
-    SDL_Event event;
-    while (true) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-                case SDL_QUIT:
-                    exit(0);
-                    break;
-                case SDL_KEYDOWN:
-                    cerr << "\nDown: " << event.key.keysym.scancode;
-                    break;
-                case SDL_KEYUP:
-                    cerr << "\nUp: " << event.key.keysym.scancode;
-                    break;
-                default: cerr << "\n.";
+    while (running){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT || e.type == SDL_KEYDOWN){
+                running = false;
             }
         }
-        SDL_Delay(100);
     }
-
-    graphics.quit();
-    return 0;
 }
-*/
+
+int main(int argc, char* argv[]){
+    SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+}
