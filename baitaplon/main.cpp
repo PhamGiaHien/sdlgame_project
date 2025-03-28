04187f5f2f75b47792f7df79e03988542209e5c7
PhamGiaHien <phamgiahien1205@gmail.com>
2025-03-28 23:04:22 +0700

    up check

--- baitaplon/main.cpp
+++ baitaplon/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 800;
+const char* WINDOW_TYPE = "Game";
 
 using namespace std;
 
-void wait_until_exit(){
+void wait_until_press_exit(){
     SDL_Event e;
     bool running = true;
 
@@ -20,5 +22,25 @@ void wait_until_exit(){
 }
 
 int main(int argc, char* argv[]){
-    SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow(WINDOW_TYPE, SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    bool running = true;
+    SDL_Event e;
+
+    while (running){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                running = false;
+            }
+        }
+        SDL_Texture *texture = IMG_LoadTexture(renderer, "Start.jpg");
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
 }
