6c494bb77c61cd0edf25db270e9fcfcaed5b6499
PhamGiaHien <phamgiahien1205@gmail.com>
2025-04-05 16:09:44 +0700

    add

---
+++ baitaplon/defs.h
@@ -0,0 +1,8 @@
+#ifndef _DEFS.H
+#define _DEFS.H
+
+const int SCREEN_WIDTH = 1000;
+const int SCREEN_HEIGHT = 800;
+const char* WINDOW_TYPE = "Game";
+
+#endif // _DEFS
--- baitaplon/main.cpp
+++ baitaplon/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
-
-const int SCREEN_WIDTH = 1000;
-const int SCREEN_HEIGHT = 800;
-const char* WINDOW_TYPE = "Game";
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include "defs.h"
 
 using namespace std;
 
-void wait_until_press_exit(){
+void wait_until_exit(){
     SDL_Event e;
     bool running = true;
 
@@ -21,24 +20,38 @@ void wait_until_press_exit(){
     }
 }
 
+
+
 int main(int argc, char* argv[]){
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        cerr << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_Window* window = SDL_CreateWindow(WINDOW_TYPE, SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+
     bool running = true;
     SDL_Event e;
 
     while (running){
         while (SDL_PollEvent(&e)){
-            if (e.type == SDL_QUIT){
+            if(e.type == SDL_QUIT){
                 running = false;
             }
         }
-        SDL_Texture *texture = IMG_LoadTexture(renderer, "Start.jpg");
+        SDL_Texture* texture = IMG_LoadTexture (renderer, "jungle.jpg");
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
     }
-    SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     IMG_Quit();
